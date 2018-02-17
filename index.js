05f3eb9caab3208cf70f76324a3c270646f0f479
Rafael Huijon <rhuijonp@gmail.com>
2018-02-17 22:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,43 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  
+  var depth = document.getElementById("grand-node");
+		var deepestChild = "";
+		if (depth.childElementCount > 0) {
+			// console.log(depth.children[0]);
+			deepestChild = "Base node element has children"; 
+			console.log(deepestChild);
+			if (depth.children[0].childElementCount > 0) {
+				// console.log(depth.children[0].children[0]);
+				deepestChild = "There is decendants at level 1";
+				console.log(deepestChild);
+				if (depth.children[0].children[0].childElementCount > 0){
+					// console.log(depth.children[0].children[0].children[0]);
+					deepestChild = "There is decendants at level 2";
+					console.log(deepestChild);
+					if (depth.children[0].children[0].children[0].childElementCount > 0) {
+						// console.log(depth.children[0].children[0].children[0].children[0]);
+						deepestChild = "There is decendants at level 4";
+						console.log(deepestChild);
+						if (depth.children[0].children[0].children[0].children[0].childElementCount > 0) {
+							// console.log(depth.children[0].children[0].children[0].children[0].firstChild);
+							deepestChild = "There is decendants at level 5";
+							console.log(deepestChild);
+						}	else {
+							deepestChild = depth.children[0].children[0].children[0].children[0];
+						}
+					} else {
+						deepestChild = depth.children[0].children[0].children[0];
+					}
+				} else {
+					deepestChild = depth.children[0].children[0];
+				}
+			} else {
+				deepestChild = depth.children[0];
+			}
+		} else {
+			deepestChild = depth;
+		}
+		return deepestChild;
   
 }
