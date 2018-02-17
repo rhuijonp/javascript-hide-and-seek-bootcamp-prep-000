8b3af4db644fb86a83fae35592db6e4b537fd167
Rafael Huijon <rhuijonp@gmail.com>
2018-02-17 22:15:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  		var rankedLists = document.getElementsByClassName('ranked-list');
+		
+		for (var nodeNumber = 0; nodeNumber < rankedLists.length; nodeNumber++) {
+			
+			for (var childNumber = 0; childNumber < rankedLists[nodeNumber].childElementCount; childNumber++) { 
+				rankedLists[nodeNumber].children[childNumber].innerHTML = parseInt(rankedLists[nodeNumber].children[childNumber].innerHTML) + n;
+
+			}
+		}
+
+}
+
+function deepestChild() {
+  
+  
+}
